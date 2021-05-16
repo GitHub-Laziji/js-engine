5ffcc55ebf13f52ddeb426a5244316ce8f5968a1
GitHub-Laziji <c_smtp@163.com>
2021-05-16 23:21:29 +0800

    init

--- src/test/java/org/laziji/commons/RegText.java
+++ src/test/java/org/laziji/commons/RegText.java
@@ -8,5 +8,11 @@ public class RegText {
     @Test
     public void test() {
         System.out.println(Token.REMARK.match("/*sasdasd\nasdsaddasd"));
+        System.out.println(Token.CONST.match("const aaa"));
+        System.out.println(Token.CONST.match("const$aaa"));
+        System.out.println(Token.CONST.match("const"));
+        System.out.println(Token.REMARK_OF_LINE.match("//asdasd\nasdsad"));
+        System.out.println(Token.REMARK_OF_LINE.match("//s"));
+        System.out.println(Token.STRING.match("\"asd\\\"sad\"asdsad"));
     }
 }
