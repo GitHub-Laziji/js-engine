3b8f37309f39d9b6b3e67b68bdbdbb21a5a2ea9a
GitHub-Laziji <c_smtp@163.com>
2021-06-14 18:51:06 +0800

    import

--- src/main/java/org/laziji/commons/js/model/node/word/basic/ArrayWordNode.java
+++ src/main/java/org/laziji/commons/js/model/node/word/basic/ArrayWordNode.java
@@ -1,12 +1,10 @@
 package org.laziji.commons.js.model.node.word.basic;
 
 import org.laziji.commons.js.consts.Token;
-import org.laziji.commons.js.model.node.BaseBracketNode;
 import org.laziji.commons.js.model.node.BasePlanNode;
 import org.laziji.commons.js.model.node.Node;
 import org.laziji.commons.js.model.node.UnitNode;
 import org.laziji.commons.js.model.node.internal.BracketContentInternalNode;
-import org.laziji.commons.js.model.node.internal.OperatorWordNameInternalNode;
 
 import java.util.Arrays;
 import java.util.List;
