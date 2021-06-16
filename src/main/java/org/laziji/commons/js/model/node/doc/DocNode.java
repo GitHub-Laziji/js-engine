563e9e07807a4d6c62b0f50a98f94b043e0672f3
GitHub-Laziji <c_smtp@163.com>
2021-06-17 00:32:44 +0800

    ListNode

--- src/main/java/org/laziji/commons/js/model/node/doc/DocNode.java
+++ src/main/java/org/laziji/commons/js/model/node/doc/DocNode.java
@@ -1,48 +1,31 @@
 package org.laziji.commons.js.model.node.doc;
 
 import org.laziji.commons.js.consts.Token;
-import org.laziji.commons.js.model.TokenUnit;
-import org.laziji.commons.js.model.node.BaseNode;
+import org.laziji.commons.js.model.node.BasePlanNode;
 import org.laziji.commons.js.model.node.Node;
+import org.laziji.commons.js.model.node.UnitNode;
 import org.laziji.commons.js.model.node.section.SectionNode;
 
-public class DocNode extends BaseNode {
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
 
-    private SectionNode section;
-    private boolean end;
+public class DocNode extends BasePlanNode {
 
     public DocNode() {
         super(null);
     }
 
     @Override
-    public Node init() {
-        section = new SectionNode(this);
-        return section.init();
-    }
-
-    @Override
-    public Node append(TokenUnit unit) throws Exception {
-        if (section.isDone()) {
-            if (unit.getToken() == Token.EOF) {
-                end = true;
-                return this;
-            }
-        }
-        if (isDone()) {
-            throw new Exception(String.format("doc over.", unit.getToken().toString()));
-        }
-        throw new Exception(String.format("[%s] is not the expected token.", unit.getToken().toString()));
-
-    }
-
-    @Override
-    public boolean isDone() {
-        return section.isDone() && end;
+    protected List<Supplier<Node>> getPlan() {
+        return Arrays.asList(
+                () -> new SectionNode(this),
+                () -> new UnitNode(this, Token.EOF)
+        );
     }
 
     @Override
     public String toString(int depth, boolean start) {
-        return section.toString(0, start);
+        return current[0].toString(0, start);
     }
 }
