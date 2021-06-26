5e59f544c1a318edffedf712c91414c2fda06b6b
GitHub-Laziji <c_smtp@163.com>
2021-06-26 23:31:29 +0800

    function runtime

--- src/main/java/org/laziji/commons/js/model/node/word/BooleanWordNode.java
+++ src/main/java/org/laziji/commons/js/model/node/word/BooleanWordNode.java
@@ -2,19 +2,14 @@ package org.laziji.commons.js.model.node.word;
 
 import org.laziji.commons.js.constant.Token;
 import org.laziji.commons.js.model.context.Context;
-import org.laziji.commons.js.model.node.BasePlanNode;
 import org.laziji.commons.js.model.node.BaseUnitNode;
 import org.laziji.commons.js.model.node.Node;
-import org.laziji.commons.js.model.node.UnitNode;
-import org.laziji.commons.js.model.node.word.WordNode;
 import org.laziji.commons.js.model.value.BooleanValue;
 import org.laziji.commons.js.model.value.Value;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Set;
 import java.util.Stack;
-import java.util.function.BiFunction;
 
 public class BooleanWordNode extends BaseUnitNode implements WordNode {
 
