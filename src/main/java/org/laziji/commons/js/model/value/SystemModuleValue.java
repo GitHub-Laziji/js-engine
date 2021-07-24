81dc309cb81c94924a08f19adff89a62a6f6c2cd
GitHub-Laziji <c_smtp@163.com>
2021-07-24 20:27:12 +0800

    import default

--- src/main/java/org/laziji/commons/js/model/value/SystemModuleValue.java
+++ src/main/java/org/laziji/commons/js/model/value/SystemModuleValue.java
@@ -1,9 +1,14 @@
 package org.laziji.commons.js.model.value;
 
+import org.laziji.commons.js.exception.RunException;
+
 public class SystemModuleValue extends ModuleValue {
 
     {
         setDefaultExportValue(new InternalFunction((caller, manager, arguments) -> {
+            if (arguments.size() < 1) {
+                throw new RunException();
+            }
             return new StringValue("SystemFunction: " + arguments.get(0).toString());
         }));
     }
